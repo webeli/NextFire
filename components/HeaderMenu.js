3371121176c04df50e5c4c22b6842ce21292e069
Elias Hansen <pe_lias@msn.com>
2017-10-24 22:05:11 +0200

    Created HeaderMenu.js

---
+++ components/HeaderMenu.js
@@ -0,0 +1,11 @@
+export default () =>
+  <div>
+    <div className="menu">Menu</div>
+    <style jsx>{`
+      .menu {
+        background: #CCC;
+        color: white;
+        height: 50px;
+      }
+    `}</style>
+  </div>
