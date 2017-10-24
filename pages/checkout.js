111c1a2a8f17a0168e1b393be471235283d09fcd
Elias Hansen <pe_lias@msn.com>
2017-10-24 22:05:45 +0200

    Adjustments on checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,1 +1,7 @@
-export default () => <div>Checkout!</div>
+import SiteLayout from '../components/SiteLayout'
+
+export default () =>
+  <div>
+    <SiteLayout title="Checkout" />
+    <div>Checkout!</div>
+  </div>
