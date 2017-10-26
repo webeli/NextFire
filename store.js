64052302e8610aff75635d369059e4f079d83f5e
Elias Hansen <pe_lias@msn.com>
2017-10-26 20:09:47 +0200

    Created store

---
+++ store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
+
+const initialState = {
+  drawerOpen: false,
+}
+
+export const actionTypes = {
+  TOGGLE_DRAWER: 'TOGGLE_DRAWER',
+}
+
+// REDUCERS
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.TOGGLE_DRAWER:
+      return Object.assign({}, state, {
+        drawerOpen: !state.drawerOpen
+      })
+    default: return state
+  }
+}
+
+export const toggleDrawer = (isServer) => dispatch => {
+  return dispatch({ type: actionTypes.TOGGLE_DRAWER })
+}
+
+export const initStore = (initialState = initialState) => {
+  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+}
