2bdc0b96d6d0358f4814e732bcd8e70e290651d7
Elias Hansen <pe_lias@msn.com>
2017-10-26 20:10:34 +0200

    Adjustments to SiteLayout

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -1,18 +1,43 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { initStore, toggleDrawer } from '../store'
+import { connect } from 'react-redux'
 import Head from 'next/head'
 import Header from './Header'
+import Drawer from './Drawer'
 
-export default ({ title, children }) =>
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <Header />
-    {children}
-    <style global jsx>{`
-      body {
-        margin: 0;
-        padding: 0;
-      }
-    `}</style>
-  </div>
+class SiteLayout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>{this.props.title}</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Drawer toggleDrawer={() => this.props.toggleDrawer()} open={this.props.drawerOpen} />
+        <Header toggleDrawer={() => this.props.toggleDrawer()} />
+        {this.props.children}
+        <style global jsx>{`
+        body {
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    drawerOpen: state.drawerOpen
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleDrawer: bindActionCreators(toggleDrawer, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SiteLayout)
