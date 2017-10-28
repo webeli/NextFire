e4768ae4a46a7389f5991450da79a251cca91efd
Elias Hansen <pe_lias@msn.com>
2017-10-28 23:14:53 +0200

    Created Cart.js

---
+++ components/Cart.js
@@ -0,0 +1,38 @@
+import Close from '../assets/svg/md-close.svg'
+import Overlay from './Overlay'
+import classnames from 'classnames';
+
+export default (props) =>
+  <div>
+    <Overlay onClick={() => props.toggleCart()} open={props.open} />
+    <div className={classnames('cart', { open: props.open })}>
+      <header>
+        Cart
+        <Close style={{ float: 'right', height: 50, color: '#FFF', fill: 'currentColor' }} onClick={() => props.toggleCart()} />
+      </header>
+      {props.children}
+    </div>
+    <style jsx>{`
+      header {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        background: #1a171b;
+        color: white;
+        height: 50px;
+      }
+      .cart {
+        position: fixed;
+        height: 100vh;
+        width: 85vw;
+        right: -95vw;
+        background: #FFF;
+        box-shadow: 1px 0px 50px 0px #000
+        z-index: 5;
+        transition: right .3s;
+      }
+      .open {
+        right: 0vw;
+      }
+  `}</style>
+  </div>
