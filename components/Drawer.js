84ec022b8e79624acb6a5548d6e44893643d41b7
Elias Hansen <pe_lias@msn.com>
2017-10-28 23:15:11 +0200

    Moved Overlay to Drawer, minor styling adjustments

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,11 +1,14 @@
 import Close from '../assets/svg/md-close.svg'
+import Overlay from './Overlay'
 import classnames from 'classnames';
 
 export default (props) =>
   <div>
+    <Overlay onClick={() => props.toggleDrawer()} open={props.open} />
     <div className={classnames('drawer', { open: props.open })}>
       <header>
         <Close style={{ height: 50, color: '#FFF', fill: 'currentColor' }} onClick={() => props.toggleDrawer()}/>
+        Drawer
       </header>
       {props.children}
     </div>
@@ -21,14 +24,14 @@ export default (props) =>
         position: fixed;
         height: 100vh;
         width: 85vw;
-        margin-left: -95vw;
+        left: -95vw;
         background: #FFF;
         box-shadow: 1px 0px 50px 0px #000
         z-index: 5;
-        transition: margin-left .3s;
+        transition: left .3s;
       }
       .open {
-        margin-left: 0vw;
+        left: 0vw;
       }
   `}</style>
   </div>
