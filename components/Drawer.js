25edd2fb276780037a9ec090389626dde4de30e2
Elias Hansen <pe_lias@msn.com>
2017-10-28 12:08:50 +0200

    Drawer animation and icons

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,17 +1,34 @@
+import Close from '../assets/svg/md-close.svg'
+import classnames from 'classnames';
+
 export default (props) =>
   <div>
-    {props.open && (
-      <div className="drawer" onClick={() => props.toggleDrawer()}>
-        <p>Drawer</p>
-      </div>
-    )}
+    <div className={classnames('drawer', { open: props.open })}>
+      <header>
+        <Close style={{ height: 50, color: '#FFF', fill: 'currentColor' }} onClick={() => props.toggleDrawer()}/>
+      </header>
+      {props.children}
+    </div>
     <style jsx>{`
+      header {
+        display: flex;
+        align-items: center;
+        background: #1a171b;
+        color: white;
+        height: 50px;
+      }
       .drawer {
         position: fixed;
         height: 100vh;
         width: 85vw;
+        margin-left: -95vw;
         background: #FFF;
+        box-shadow: 1px 0px 50px 0px #000
         z-index: 5;
+        transition: margin-left .3s;
+      }
+      .open {
+        margin-left: 0vw;
       }
   `}</style>
   </div>
