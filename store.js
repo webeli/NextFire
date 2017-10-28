52efa5fd345406a3cbf49d225097f37cf2124321
Elias Hansen <pe_lias@msn.com>
2017-10-28 23:14:19 +0200

    Created toggleCart

--- store.js
+++ store.js
@@ -4,10 +4,12 @@ import thunk from 'redux-thunk'
 
 const initialState = {
   drawerOpen: false,
+  cartOpen: false,
 }
 
 export const actionTypes = {
   TOGGLE_DRAWER: 'TOGGLE_DRAWER',
+  TOGGLE_CART: 'TOGGLE_CART',
 }
 
 // REDUCERS
@@ -17,12 +19,20 @@ export const reducer = (state = initialState, action) => {
       return Object.assign({}, state, {
         drawerOpen: !state.drawerOpen
       })
+    case actionTypes.TOGGLE_CART:
+      return Object.assign({}, state, {
+        cartOpen: !state.cartOpen
+      })
     default: return state
   }
 }
 
 export const toggleDrawer = (isServer) => dispatch => {
   return dispatch({ type: actionTypes.TOGGLE_DRAWER })
+}
+
+export const toggleCart = (isServer) => dispatch => {
+  return dispatch({ type: actionTypes.TOGGLE_CART })
 }
 
 export const initStore = (initialState = initialState) => {
