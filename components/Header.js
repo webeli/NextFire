5fbe9ddbbb8ecbd66305186983546d587bcb2d72
Elias Hansen <pe_lias@msn.com>
2017-10-28 12:08:04 +0200

    Added MenuIcon to Header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,17 @@
 import HeaderMenu from './HeaderMenu'
+import MenuIcon from '../assets/svg/md-menu.svg'
 
 export default (props) =>
   <div>
     <header>
-      <span onClick={() => props.toggleDrawer()}>DrawerIcon</span>
+      <MenuIcon style={{ height: 50, color: '#FFF', fill: 'currentColor' }} onClick={() => props.toggleDrawer()} />
       <span>Header</span>
     </header>
     <HeaderMenu />
     <style jsx>{`
       header {
-        font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+        display: flex;
+        align-items: center;
         background: #1a171b;
         color: white;
         height: 50px;
