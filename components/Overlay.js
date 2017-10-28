da4988113021dc8e4cb4b7eb517c8d99be908a16
Elias Hansen <pe_lias@msn.com>
2017-10-28 23:15:32 +0200

    Overlay transition adjustments

--- components/Overlay.js
+++ components/Overlay.js
@@ -2,22 +2,22 @@ import Close from '../assets/svg/md-close.svg'
 import classnames from 'classnames';
 
 export default (props) =>
-    <div>
-        <div className={classnames('overlay', { open: props.open })} onClick={() => props.onClick()} />
-        <style jsx>{`
+  <div>
+    <div className={classnames('overlay', { open: props.open })} onClick={() => props.onClick()} />
+    <style jsx>{`
       .overlay {
         position: fixed;
         height: 0;
         width: 0;
         background: #000;
         z-index: 4;
-        opacity: 0.5;
-        transition: margin-left .3s;
+        opacity: 0;
+        transition: opacity .3s;
       }
       .open {
+        opacity: 0.2;
         height: 100vh;
         width: 100vw;
-        margin-left: 0vw;
       }
   `}</style>
-    </div>
+  </div>
