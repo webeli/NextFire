4aa15c4b823dbddc51344abc81129c50476959bc
Elias Hansen <pe_lias@msn.com>
2017-10-31 21:39:53 +0100

    Using getInitialProps on index to get categories

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,17 @@ import { initStore } from '../store'
 import SiteLayout from '../components/SiteLayout'
 
 class Home extends React.Component {
+  static async getInitialProps({ req }) {
+    const res = await fetch('https://firecms-1.firebaseio.com/categories.json')
+    console.log('res', res);
+    const json = await res.json()
+    return { categories: json }
+  }
+
   render() {
+    console.log('Home', this.props);
     return (
-      <SiteLayout title="Home">
+      <SiteLayout title="Home" categories={this.props.categories}>
         <div>Welcome to next.js!</div>
       </SiteLayout>
     )
