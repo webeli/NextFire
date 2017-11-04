5554061b20e24f4c0cad189ec4837b02ec651f48
Elias Hansen <pe_lias@msn.com>
2017-11-04 14:34:54 +0100

    Fixed and cleanup index

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ import SiteLayout from '../components/SiteLayout'
 class Home extends React.Component {
   static async getInitialProps({ req }) {
     const res = await fetch('https://firecms-1.firebaseio.com/categories.json')
-    console.log('res', res);
     const json = await res.json()
     return { categories: json }
   }
 
   render() {
-    console.log('Home', this.props);
     return (
-      <SiteLayout title="Home" categories={this.props.categories}>
+      <SiteLayout title='Home' currentPage={this.props}>
         <div>Welcome to next.js!</div>
       </SiteLayout>
     )
